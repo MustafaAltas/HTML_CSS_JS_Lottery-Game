48f0b9b1083de1871dd704b335ed4926367e64d8
D1183-Mustafa <93746275+D1183-Mustafa@users.noreply.github.com>
2022-02-15 05:12:03 +0300

    şanslı numara ataması yapıldı

--- app.js
+++ app.js
@@ -3,11 +3,27 @@
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click",(event) => {
+
+    //Kullanıcıdan numaraları bir array içinde tutma işlemi
     let kullanıcı_girdi = document.querySelectorAll(".girdi");
     let girilen_numaralar = [];
     for (let i = 0; i < kullanıcı_girdi.length; i++) {
         girilen_numaralar.push(kullanıcı_girdi[i].value);
         
     }
-    console.log(girilen_numaralar);
+
+
+
+    // Şanslı numaraların atama işlemini gerçekleştirmek için döngü kuruldu.
+    let şanslı_numaralar = document.querySelectorAll(".numara");
+    console.log(şanslı_numaralar);
+    let şanslı_numaralar_array = [];
+    for (let i = 0; i < şanslı_numaralar.length; i++) {
+        let numRandom = Math.floor(Math.random() * 99) + 1;
+        şanslı_numaralar[i].innerHTML = numRandom;
+        şanslı_numaralar_array.push(numRandom);
+        
+    }
+
+
 })
